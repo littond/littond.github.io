32541f5abd9a607ce97e7d7ff05f263963606a83
littond <114114709+littond@users.noreply.github.com>
2025-02-17 04:36:45 +0000

    Deploying to gh-pages from @ littond/littond.github.io@e67c96c5ec644751c58bfeba1420595500f96f3f 🚀

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import { App } from './components/app'
-import React from 'react'
-
-createRoot(document.getElementById('root')).render(
-  <div>
-    <App />
-  </div>
-)
