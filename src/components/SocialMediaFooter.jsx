8d7444bb38f88d21591c459a1cc8cee234ca4dbd
littond <littond@msoe.edu>
2025-03-01 23:35:32 -0600

    added social media footer

---
+++ src/components/SocialMediaFooter.jsx
@@ -0,0 +1,32 @@
+import "../styles/SocialMediaFooter.css";
+import ghLogo from '/github-mark.svg'
+import lnLogo from '/LI-In-Bug.png'
+
+export const SocialMediaFooter = () => {
+    const media = [
+        {
+            name: "Github",
+            site: "https://github.com/littond",
+            icon: ghLogo,
+        },
+        {
+            name: "LinkedIn",
+            site: "https://www.linkedin.com/in/david-litton/",
+            icon: lnLogo,
+        }
+    ];
+    return (
+        <div className="social-media-footer">
+            {media.map((platform) => (
+                <a key={platform.name} href={platform.site} target="_blank" rel="noopener noreferrer" className="social-link">
+                    <img
+                        src={platform.icon}
+                        alt={platform.name}
+                        className="social-icon"
+                    />
+                    <span className="tooltip">{platform.name}</span>
+                </a>
+            ))}
+        </div>
+    );
+}
--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,11 +1,13 @@
 import { About } from "./about";
 import { Experience } from "./experience";
+import { SocialMediaFooter } from "./SocialMediaFooter"
 
 export const App = () => {
     return (
         <div>
             <About />
             <Experience />
+            <SocialMediaFooter />
         </div>
     );
 }
