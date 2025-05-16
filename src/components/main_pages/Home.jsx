1f558d0e6b74dd5c5b1c1a68b66d14a20ef292f9
littond <littond@msoe.edu>
2025-05-15 22:06:10 -0500

    chore: started styling the bio

--- src/components/main_pages/Home.jsx
+++ src/components/main_pages/Home.jsx
@@ -1,12 +1,12 @@
 import { Experience } from "../sub_pages/Experience";
-import { HomeHeroSection } from "../sub_pages/HomeHeroSection";
+import { SiteTitleBanner } from "../sub_pages/SiteTitleBanner";
 import { Bio } from "../sub_pages/Bio";
 import "../../styles/main_pages/Home.css";
 
 export const Home = () => {
     return (
         <div className="home-container">
-            <HomeHeroSection />
+            <SiteTitleBanner />
             <Bio page="home"/>
             <Experience limit={1} />
         </div>
---
+++ src/components/sub_pages/SiteTitleBanner.jsx
@@ -0,0 +1,10 @@
+import "../../styles/sub_pages/SiteTitleBanner.css";
+
+export const SiteTitleBanner = () => {
+    return (
+        <div className="site-title-banner">
+            <h1>HELLO!</h1>
+            <h2>Welcome to My Portfolio Site</h2>
+        </div>
+    );
+};
